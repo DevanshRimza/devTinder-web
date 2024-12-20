9aaeb634511f0acea8c599b8163c030acb130360
Devansh Rimza <rimzadevansh@gmail.com>
2024-12-20 12:38:19 +0530

    Fixed Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,14 @@
-
-
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
-const config = {
+
+const daisyui = require("daisyui");
+
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
 };
-
-export default config;
-
 
 
 
